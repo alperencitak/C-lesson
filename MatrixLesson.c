c70d122fcda9695a8ddd388985b0ecd0d585dbd0
Alperen Çıtak <citak_alperen@outlook.com>
2023-10-15 00:26:17 +0300

    MatrixLesson

---
+++ MatrixLesson.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	
+	char kayitisim[30];
+	char kayitsifre[20];
+	char isim[50];
+	char sifre[20];
+	
+	
+	printf("**Kullanici adi ve sifre belirleyin.\n...\n\n");
+	
+	kayitisimloc:
+	printf("Kullanici adi belirleyin : ");
+	scanf("%s", kayitisim);
+	if (strlen(kayitisim) > 30){
+		printf("Kullanici adi 30 karakterden buyuk olamaz! Tekrar deneyin.\n\n");
+		goto kayitisimloc;
+	}
+	
+	kayitsifreloc:
+	printf("Sifre belirleyin : ");
+	scanf("%s", kayitsifre);
+	if (strlen(kayitsifre) > 20){
+		printf("Sifreniz 20 karakterden buyuk olamaz! Tekrar deneyin.\n\n");
+		goto kayitsifreloc;
+	}
+	if (strlen(kayitsifre) < 5){
+		printf("Sifreniz en az 5 karakter olmali! Tekrar deneyin.\n\n");
+		goto kayitsifreloc;
+	}
+
+	printf("\n\n");
+	
+	
+	printf("**Kayit basarili! Lutfen giris yapin.\n...\n\n");
+	
+	girisloc:
+	printf("Kullanici adi : ");
+	scanf("%s", isim);
+	
+	printf("Sifre : ");
+	scanf("%s", sifre);
+	
+	if (strcmp(isim, kayitisim) != 0 || strcmp(sifre, kayitsifre) != 0){
+		printf("\nKullanici adi veya sifre yanlis! Tekrar deneyin\n\n");
+		goto girisloc;
+	}
+	
+	printf("\n**Giris basarili!");
+
+	return 0;
+}
